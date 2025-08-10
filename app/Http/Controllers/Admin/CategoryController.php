4c6702b8fa35fffbeb6f90ff65205945bcf55a0b
Mahmoud_Magdy <mahmoud30204050@gmail.com>
2025-08-10 23:05:25 +0300

    .

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\DataTables\CategoryDataTable;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\CategoryCreateRequest;
+use App\Http\Requests\Admin\CategoryUpdateRequest;
+use App\Models\Category;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Str;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(CategoryDataTable $dataTable)
+    {
+        return $dataTable->render('admin.product.category.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create() : View
+    {
+        return view('admin.product.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryCreateRequest $request) : RedirectResponse
+    {
+
+        $data = $request->validated();
+        $data['slug'] = Str::slug($data['name']);
+
+        Category::create($data);
+
+        toastr()->success('Created Successfully');
+
+        return to_route('admin.category.index');
+
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id) : View
+    {
+        $category = Category::findOrFail($id);
+        return view('admin.product.category.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryUpdateRequest $request, string $id)
+    {
+        $data = $request->validated();
+
+        $category = Category::findOrFail($id);
+
+        $data['slug'] = Str::slug($data['name']);
+
+        $category->update($data);
+
+        toastr()->success('Updated Successfully');
+
+        return to_route('admin.category.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try{
+            Category::findOrFail($id)->delete();
+            return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
+        }catch(\Exception $e){
+            return response(['status' => 'error', 'message' => 'something went wrong!']);
+        }
+    }
+}
