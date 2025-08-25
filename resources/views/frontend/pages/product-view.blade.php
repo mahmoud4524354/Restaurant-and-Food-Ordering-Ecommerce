7152bff03828cfc38e9ecbdb2026118e9fee4e26
Mahmoud_Magdy <mahmoud30204050@gmail.com>
2025-08-25 14:51:33 +0300

    Add to sidebar-cart feature

--- resources/views/frontend/pages/product-view.blade.php
+++ resources/views/frontend/pages/product-view.blade.php
@@ -90,6 +90,7 @@
                                             </label>
                                         </div>
                                     @endforeach
+
                                 </div>
                             @endif
 
@@ -129,7 +130,7 @@
 
                         <ul class="details_button_area d-flex flex-wrap">
                             @if ($product->quantity === 0)
-                                <li><a class="common_btn bg-danger" href="javascript:">Stock Out</a></li>
+                                <li><a class="common_btn bg-danger" href="javascript:;">Stock Out</a></li>
                             @else
                                 <li><a class="common_btn v_submit_button" href="#">add to cart</a></li>
                             @endif
@@ -163,112 +164,63 @@
                             <div class="tab-pane fade" id="pills-contact" role="tabpanel"
                                  aria-labelledby="pills-contact-tab" tabindex="0">
                                 <div class="fp__review_area">
-                                    <div class="row">
-                                        <div class="col-lg-8">
-                                            <div class="fp__comment pt-0 mt_20">
 
-                                            </div>
-
-                                        </div>
-                                        @auth
-                                            <div class="col-lg-4">
-                                                <div class="fp__post_review">
-                                                    <h4>write a Review</h4>
-                                                    <form action="">
-                                                        @csrf
-                                                        <div class="row">
-                                                            <div class="col-xl-12 mt-3">
-                                                                <label> Choose a rating</label>
-                                                                <select name="rating" id="rating_input"
-                                                                        class="form-control ">
-                                                                    <option value="5">5</option>
-                                                                    <option value="4">4</option>
-                                                                    <option value="3">3</option>
-                                                                    <option value="2">2</option>
-                                                                    <option value="1">1</option>
-                                                                </select>
-                                                                <input type="hidden" name="product_id"
-                                                                       value="{{ $product->id }}">
-                                                            </div>
-
-                                                            <div class="col-xl-12">
-                                                                <label for="">Review</label>
-                                                                <textarea style="margin-top: 2px" name="review" rows="3"
-                                                                          placeholder="Write your review"></textarea>
-                                                            </div>
-                                                            <div class="col-12">
-                                                                <button class="common_btn" type="submit">submit
-                                                                    review
-                                                                </button>
-                                                            </div>
-                                                        </div>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        @else
-                                            <div class="col-lg-4">
-                                                <h4>write a Review</h4>
-                                                <div class="alert alert-warning mt-4">Please login first to add
-                                                    review.
-                                                </div>
-                                            </div>
-                                        @endauth
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        @if (count($relatedProducts) > 0)
-            <div class="fp__related_menu mt_90 xs_mt_60">
-                <h2>related item</h2>
-                <div class="row related_product_slider">
-                    @foreach ($relatedProducts as $relatedProduct)
-                        <div class="col-xl-3 wow fadeInUp" data-wow-duration="1s">
-                            <div class="fp__menu_item">
-                                <div class="fp__menu_item_img">
-                                    <img src="{{ asset($relatedProduct->thumb_image) }}"
-                                         alt="{{ $relatedProduct->name }}" class="img-fluid w-100">
-                                    <a class="category" href="#">{{ @$relatedProduct->category->name }}</a>
-                                </div>
-                                <div class="fp__menu_item_text">
-                                    <p class="rating">
+
+            @if (count($relatedProducts) > 0)
+                <div class="fp__related_menu mt_90 xs_mt_60">
+                    <h2>related items</h2>
+                    <div class="row related_product_slider">
+                        @foreach ($relatedProducts as $relatedProduct)
+                            <div class="col-xl-3 wow fadeInUp" data-wow-duration="1s">
+                                <div class="fp__menu_item">
+                                    <div class="fp__menu_item_img">
+                                        <img src="{{ asset($relatedProduct->thumb_image) }}"
+                                             alt="{{ $relatedProduct->name }}" class="img-fluid w-100">
+                                        <a class="category" href="#">{{ @$relatedProduct->category->name }}</a>
+                                    </div>
+                                    <div class="fp__menu_item_text">
                                         <p class="rating">
-                                            @for ($i = 1; $i <= $relatedProduct->reviews_avg_rating; $i++)
-                                                <i class="fas fa-star"></i>
-                                            @endfor
+                                        @if ($relatedProduct->reviews_avg_rating)
+                                            <p class="rating">
+                                                @for ($i = 1; $i <= $relatedProduct->reviews_avg_rating; $i++)
+                                                    <i class="fas fa-star"></i>
+                                                @endfor
 
-                                            <span>({{ $relatedProduct->reviews_count }})</span>
-                                        </p>
-
-
-                                        <a class="title"
-                                           href="{{ route('product.show', $relatedProduct->slug) }}">{!! $relatedProduct->name !!}</a>
-                                        <h5 class="price">
-                                            @if ($relatedProduct->offer_price > 0)
-                                                {{ currencyPosition($relatedProduct->offer_price) }}
-                                                <del>{{ currencyPosition($relatedProduct->price) }}</del>
-                                            @else
-                                                {{ currencyPosition($relatedProduct->price) }}
+                                                <span>({{ $relatedProduct->reviews_count }})</span>
+                                            </p>
                                             @endif
-                                        </h5>
-                                        <ul class="d-flex flex-wrap justify-content-center">
-                                            <li><a href="javascript:"
-                                                   onclick="loadProductModal('{{ $relatedProduct->id }}')"><i
-                                                        class="fas fa-shopping-basket"></i></a></li>
-                                            <li><a href="#"><i class="fal fa-heart"></i></a></li>
-                                            <li><a href="#"><i class="far fa-eye"></i></a></li>
-                                        </ul>
+                                            </p>
+                                            <a class="title"
+                                               href="{{ route('product.show', $relatedProduct->slug) }}">{!! $relatedProduct->name !!}</a>
+                                            <h5 class="price">
+                                                @if ($relatedProduct->offer_price > 0)
+                                                    {{ currencyPosition($relatedProduct->offer_price) }}
+                                                    <del>{{ currencyPosition($relatedProduct->price) }}</del>
+                                                @else
+                                                    {{ currencyPosition($relatedProduct->price) }}
+                                                @endif
+                                            </h5>
+                                            <ul class="d-flex flex-wrap justify-content-center">
+                                                <li><a href="javascript:;"
+                                                       onclick="loadProductModal('{{ $relatedProduct->id }}')"><i
+                                                            class="fas fa-shopping-basket"></i></a></li>
+                                                <li><a href="#"><i class="fal fa-heart"></i></a></li>
+                                                <li><a href="#"><i class="far fa-eye"></i></a></li>
+                                            </ul>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
 
+                    </div>
                 </div>
-            </div>
-            @endif
+            @endif        </div>
     </section>
     <!--=============================
         MENU DETAILS END
@@ -276,3 +228,111 @@
 
 @endsection
 
+@push('scripts')
+    <script>
+        $(document).ready(function () {
+            $('.v_product_size').prop('checked', false);
+            $('.v_product_option').prop('checked', false);
+            $('#v_quantity').val(1);
+
+            $('.v_product_size').on('change', function () {
+                v_updateTotalPrice()
+            });
+
+            $('.v_product_option').on('change', function () {
+                v_updateTotalPrice()
+            });
+
+            // Event handlers for increment and decrement buttons
+            $('.v_increment').on('click', function (e) {
+                e.preventDefault()
+
+                let quantity = $('#v_quantity');
+                let currentQuantity = parseFloat(quantity.val());
+                quantity.val(currentQuantity + 1);
+                v_updateTotalPrice()
+            })
+
+            $('.v_decrement').on('click', function (e) {
+                e.preventDefault()
+
+                let quantity = $('#v_quantity');
+                let currentQuantity = parseFloat(quantity.val());
+                if (currentQuantity > 1) {
+                    quantity.val(currentQuantity - 1);
+                    v_updateTotalPrice()
+                }
+            })
+
+
+            // Function to update the total price base on seelected options
+            function v_updateTotalPrice() {
+                let basePrice = parseFloat($('.v_base_price').val());
+                let selectedSizePrice = 0;
+                let selectedOptionsPrice = 0;
+                let quantity = parseFloat($('#v_quantity').val());
+
+                // Calculate the selected size price
+                let selectedSize = $('.v_product_size:checked');
+                if (selectedSize.length > 0) {
+                    selectedSizePrice = parseFloat(selectedSize.data("price"));
+                }
+
+                // Calculate selected options price
+                let selectedOptions = $('.v_product_option:checked');
+                $(selectedOptions).each(function () {
+                    selectedOptionsPrice += parseFloat($(this).data("price"));
+                })
+
+                // Calculate the total price
+                let totalPrice = (basePrice + selectedSizePrice + selectedOptionsPrice) * quantity;
+                $('#v_total_price').text("{{ config('settings.site_currency_icon') }}" + totalPrice);
+            }
+
+            $('.v_submit_button').on('click', function (e) {
+                e.preventDefault();
+                $("#v_add_to_cart_form").submit();
+            })
+
+            // Add to cart function
+            $("#v_add_to_cart_form").on('submit', function (e) {
+                e.preventDefault();
+
+                // Validation
+                let selectedSize = $(".v_product_size");
+                if (selectedSize.length > 0) {
+                    if ($(".v_product_size:checked").val() === undefined) {
+                        toastr.error('Please select a size');
+                        console.error('Please select a size');
+                        return;
+                    }
+                }
+
+                let formData = $(this).serialize();
+                $.ajax({
+                    method: 'POST',
+                    url: '{{ route("add-to-cart") }}',
+                    data: formData,
+                    beforeSend: function () {
+                        $('.v_submit_button').attr('disabled', true);
+                        $('.v_submit_button').html(
+                            '<span class="spinner-border spinner-border-sm text-light" role="status" aria-hidden="true"></span> Loading...'
+                        )
+                    },
+                    success: function (response) {
+                        updateSidebarCart();
+                        toastr.success(response.message);
+                    },
+                    error: function (xhr, status, error) {
+                        let errorMessage = xhr.responseJSON.message;
+                        toastr.error(errorMessage);
+                    },
+                    complete: function () {
+                        $('.v_submit_button').html('Add to Cart');
+                        $('.v_submit_button').attr('disabled', false);
+                    }
+                })
+            })
+        })
+    </script>
+@endpush
